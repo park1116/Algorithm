836a75b7b4ce10bc0f837b84e2f6384078842980
park1116 <55124370+park1116@users.noreply.github.com>
2020-03-20 14:45:12 +0900

    Add files via upload

---
+++ 18311.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int N = input.nextInt();
+      long K = input.nextLong();
+      int[] course = new int[N + 1];
+      for (int i = 0; i < N; i++) {
+         course[i] = input.nextInt();
+      }
+      int position = 0;
+      long result = 0;
+      long length = 0;
+      for (int i = 0; i < N; i++) {
+         length += course[i];
+      }
+      while (result <= K) {
+         if (position == N)
+            break;
+         result += course[position++];
+      }
+      while (result <= K) {
+         result += course[--position];
+      }
+      if (length > K) {
+         System.out.println(position);
+      } else {
+         System.out.println(position + 1);
+      }
+   }
+}
