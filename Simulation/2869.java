9e3d485f12d00b62f6be8be618cb607a7ac6fba9
park1116 <55124370+park1116@users.noreply.github.com>
2020-10-10 12:28:56 +0900

    Add files via upload

---
+++ Simulation/2869.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		int day = 1;
+		if((V-A)%(A-B) != 0) 
+			day += (V-A)/(A-B) + 1;
+		else
+			day += (V-A)/(A-B);
+			
+		System.out.println(day);
+	}
+}
