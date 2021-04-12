158eb3a726047b6503b33b784478a34e9dfde209
park1116 <55124370+park1116@users.noreply.github.com>
2021-04-13 00:10:45 +0900

    Add files via upload

--- Simulation/2178.java
+++ Simulation/2178.java
@@ -1,59 +1,48 @@
-package Main.Simulation;
-
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Point {
-	int x;
-	int y;
-
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
 public class Main {
-	static int N, M;
-	static int[][] maze;
-	static boolean[][] visited;
-	static int[] dx = { 0, 1, 0, -1 };
-	static int[] dy = { -1, 0, 1, 0 };
+	static boolean[][] visit;
+	static int[][] map;
+	static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, -1, 0, 1 };
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		N = input.nextInt();
-		M = input.nextInt();
-		maze = new int[N][M];
-		visited = new boolean[N][M];
-		String[] str = new String[N];
-		for (int i = 0; i < N; i++) {
-			str[i] = input.next();
-			for (int j = 0; j < M; j++) {
-				maze[i][j] = str[i].charAt(j) - 48;
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		map = new int[N][M];
+		visit = new boolean[N][M];
+		for (int y = 0; y < N; y++) {
+			String arr = sc.next();
+			for (int x = 0; x < M; x++) {
+				map[y][x] = arr.charAt(x) - 48;
 			}
 		}
-		bfs(0, 0);
-		System.out.println(maze[N - 1][M - 1]);
+		count(0, 0);
+		System.out.println(map[N - 1][M - 1]);
 	}
 
-	public static void bfs(int x, int y) {
-		Queue<Point> q = new LinkedList<Point>();
-		q.add(new Point(x, y));
-		while (!q.isEmpty()) {
-			Point now = q.remove();
+	public static void count(int x, int y) {
+		Queue<Integer> queueX = new LinkedList<Integer>();
+		Queue<Integer> queueY = new LinkedList<Integer>();
+		queueX.add(x);
+		queueY.add(y);
+		while (!queueX.isEmpty() && !queueY.isEmpty()) {
+			int nowX = queueX.poll();
+			int nowY = queueY.poll();
 			for (int i = 0; i < 4; i++) {
-				int nextX = now.x + dx[i];
-				int nextY = now.y + dy[i];
-				if (nextX < 0 || nextY < 0 || nextX > M - 1 || nextY > N - 1)
-					continue;
-				if (visited[nextY][nextX])
-					continue;
-				if (maze[nextY][nextX] != 0) {
-					q.add(new Point(nextX, nextY));
-					visited[nextY][nextX] = true;
-					maze[nextY][nextX] = maze[now.y][now.x] + 1;
+				int nextX = nowX + dx[i];
+				int nextY = nowY + dy[i];
+				if (nextX < map[0].length && nextY < map.length && nextX >= 0 && nextY >= 0) {
+					if (map[nextY][nextX] != 0 && !visit[nextY][nextX]) {
+						queueX.add(nextX);
+						queueY.add(nextY);
+						visit[nextY][nextX] = true;
+						map[nextY][nextX] = map[nowY][nowX] + 1;
+					}
 				}
 			}
 		}
