029a9fd182272398ddbf7b6dce8fb237008870d3
park1116 <55124370+park1116@users.noreply.github.com>
2021-05-03 23:35:39 +0900

    Add files via upload

---
+++ Simulation/20055.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, K;
+	static int[] A;
+	static boolean[] robot;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    N = Integer.parseInt(st.nextToken());
+	    K = Integer.parseInt(st.nextToken());
+		A = new int[2 * N];
+		robot = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i <= 2 * N - 1; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		int result = 1;
+		while(true) {
+			first();
+			second();
+			third();
+			if(fourth() == -1) {
+				result++;
+			} else {
+				bw.write(result+"\n");
+				break;
+			}
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+	public static void first() {
+		int upSideTemp = A[N - 1];
+		int downSidTemp = A[2 * N - 1];
+		for (int i = N - 1; i >= 1; i--) {
+			robot[i] = robot[i - 1];
+			A[i] = A[i - 1];
+			A[N + i] = A[N + i - 1];
+		}
+		robot[0] = false;
+		A[N] = upSideTemp;
+		A[0] = downSidTemp;
+		if (robot[N - 1])
+			robot[N - 1] = false;
+	}
+
+	public static void second() {
+		for (int i = N - 2; i >= 1; i--) {
+			if (robot[i] && !robot[i + 1] && A[i + 1] >= 1) {
+				robot[i + 1] = true;
+				robot[i] = false;
+				A[i + 1]--;
+			}
+		}
+	}
+
+	public static void third() {
+		if (A[0] != 0) {
+			robot[0] = true;
+			A[0]--;
+		}
+	}
+
+	public static int fourth() {
+		int result = 0;
+		for (int i = 0; i <= 2 * N - 1; i++) {
+			if (A[i] == 0) {
+				result++;
+			}
+			if (result == K) {
+				return result;
+			} 
+		}
+		return -1;
+	}
+}
